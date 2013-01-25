be6d4328278a5af691811b95919c2e805f457dd0
Jared Verdi <jared@jaredverdi.com>
2013-01-25 18:02:06 -0500

    new drawing utilities file

---
+++ TLCGDrawUtilities.h
@@ -0,0 +1,11 @@
+//
+//  TLCGDrawUtilities.h
+//  behance-sdk
+//
+//  Created by Thumb Labs on 1/25/13.
+//  Copyright (c) 2013 Behance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void draw1PxStroke(CGContextRef context, CGPoint startPoint, CGPoint endPoint, CGColorRef color);
---
+++ TLCGDrawUtilities.m
@@ -0,0 +1,21 @@
+//
+//  TLCGDrawUtilities.m
+//  behance-sdk
+//
+//  Created by Thumb Labs on 1/25/13.
+//  Copyright (c) 2013 Behance. All rights reserved.
+//
+
+#import "TLCGDrawUtilities.h"
+
+void draw1PxStroke(CGContextRef context, CGPoint startPoint, CGPoint endPoint, CGColorRef color)
+{
+    CGContextSaveGState(context);
+    CGContextSetLineCap(context, kCGLineCapSquare);
+    CGContextSetStrokeColorWithColor(context, color);
+    CGContextSetLineWidth(context, 1.0);
+    CGContextMoveToPoint(context, startPoint.x + 0.5, startPoint.y + 0.5);
+    CGContextAddLineToPoint(context, endPoint.x + 0.5, endPoint.y + 0.5);
+    CGContextStrokePath(context);
+    CGContextRestoreGState(context);
+}
